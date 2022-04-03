fd54d06b3e883a2f5ceccf6d8fd4c10fb9952885
Frank <franklaercio@gmail.com>
2022-04-03 04:49:40 -0300

    Adding hello world spring batch

---
+++ simple-batch/src/main/java/com/github/simplebatch/BatchConfig.java
@@ -0,0 +1,2 @@
+package com.github.simplebatch;public class BatchConfig {
+}
---
+++ spring-batch-example/src/main/java/com/github/batch/BatchConfig.java
@@ -0,0 +1,41 @@
+package com.github.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+  private final JobBuilderFactory jobBuilderFactory;
+
+  private final StepBuilderFactory stepBuilderFactory;
+
+  public BatchConfig(
+      JobBuilderFactory jobBuilderFactory,
+      StepBuilderFactory stepBuilderFactory) {
+    this.jobBuilderFactory = jobBuilderFactory;
+    this.stepBuilderFactory = stepBuilderFactory;
+  }
+
+  @Bean
+  public Job printHelloWorldJob() {
+    return jobBuilderFactory.get("helloWorldJob").start(printHelloWorldStep()).build();
+  }
+
+  public Step printHelloWorldStep() {
+    return stepBuilderFactory.get("helloWorldStep").tasklet((stepContribution, chunkContext) -> {
+      System.out.println("Hello World!");
+      return RepeatStatus.FINISHED;
+    }).build();
+  }
+}
