8893d4f581372315d82d3c6248350ef74443a377
Frank <franklaercio@gmail.com>
2022-04-03 19:28:37 -0300

    Reading job arguments and multiples executions

--- spring-batch-example/src/main/java/com/github/batch/BatchConfig.java
+++ spring-batch-example/src/main/java/com/github/batch/BatchConfig.java
@@ -2,13 +2,14 @@ package com.github.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,13 +30,19 @@ public class BatchConfig {
 
   @Bean
   public Job printHelloWorldJob() {
-    return jobBuilderFactory.get("helloWorldJob").start(printHelloWorldStep()).build();
+    return jobBuilderFactory.get("helloWorldJob").start(printHelloWorldStep()).incrementer(new RunIdIncrementer()).build();
   }
 
   public Step printHelloWorldStep() {
-    return stepBuilderFactory.get("helloWorldStep").tasklet((stepContribution, chunkContext) -> {
-      System.out.println("Hello World!");
+    return stepBuilderFactory.get("helloWorldStep").tasklet(printHelloWorldTasklet(null)).build();
+  }
+
+  @Bean
+  @StepScope
+  public Tasklet printHelloWorldTasklet(@Value("#{jobParameters['nome']}") String name) {
+    return (stepContribution, chunkContext) -> {
+      System.out.println("Hello World, " + name + "!");
       return RepeatStatus.FINISHED;
-    }).build();
+    };
   }
 }
